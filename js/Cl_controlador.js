12543dd33f7f2ff41dfaaad8c703747a7f0f18b7
isanchezucla <1001.31407194.ucla@gmail.com>
2025-04-03 22:20:10 -0400

    Respaldo Inicial

---
+++ js/Cl_controlador.js
@@ -0,0 +1,22 @@
+export default class Cl_controlador {
+    constructor(modelo, vista) {
+        this.modelo = modelo;
+        this.vista = vista;
+    }
+    procesarFlores() {
+        this.modelo.procesarRamo(this.vista.iniciarFlores(), this.vista.iniciarFruta());
+        this.vista.reporteFlores(
+            this.modelo.calcularTotal(),
+            this.modelo.calcularPorcentajeFloresNaturalesMediano(), 
+            this.modelo.totalFloresNaturalesMediano(), 
+            this.modelo.totalFloresNaturales());
+    }
+    procesarFruta() {
+        this.modelo.procesarRamo(this.vista.iniciarFruta(), this.vista.iniciarFlores());
+        this.vista.reporteFruta(
+            this.modelo.calcularTotal(),
+            this.modelo.calcularPorcentajeFloresNaturalesMediano(), 
+            this.modelo.totalFloresNaturalesMediano(), 
+            this.modelo.totalFloresNaturales());
+    }
+}
